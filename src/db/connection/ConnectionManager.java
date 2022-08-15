e908f7571348c1133ebc2ae7b6e9d5923c910728
nsol24 <nursolehah24mzul@gmail.com>
2022-08-15 11:43:34 +0800

    dbts

--- src/db/connection/ConnectionManager.java
+++ src/db/connection/ConnectionManager.java
@@ -9,9 +9,9 @@ public class ConnectionManager {
     //private static final String DB_USER = "root";
     //public static final String DB_PASSWORD = "";
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String DB_CONNECTION = "jdbc:mysql://us-cdbr-east-06.cleardb.net:3306/heroku_87ad258e7779bb6";
-	private static final String DB_USER = "b742b6bdbf6124";
-	public static final String DB_PASSWORD = "697d9973";
+	private static final String DB_CONNECTION = "jdbc:mysql://us-cdbr-east-06.cleardb.net:3306/heroku_7bb9908b8572b7e";
+	private static final String DB_USER = "b54a7607667a7f";
+	public static final String DB_PASSWORD = "213f94a3";
     public static Connection getConnection() {
         try {
             Class.forName(DB_DRIVER); //1.load the driver
