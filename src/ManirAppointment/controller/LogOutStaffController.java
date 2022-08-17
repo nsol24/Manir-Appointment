af1fa82d2f4f7b17e98e1a3f03456888fdc095af
nsol24 <nursolehah24mzul@gmail.com>
2022-08-18 01:09:59 +0800

    my app

--- src/ManirAppointment/controller/LogOutStaffController.java
+++ src/ManirAppointment/controller/LogOutStaffController.java
@@ -31,6 +31,7 @@ public class LogOutStaffController extends HttpServlet {
 		try {
 			HttpSession session = request.getSession(true);
 			session.setAttribute("sessionId", null);
+			
 			session.invalidate();
 			response.sendRedirect("staff-login.jsp");
 		}
